b0263ee5c45b4a7d80b24c45accc6e7124e437e3
Nazar Mihonko <migonkonazar@gmail.com>
2026-04-09 18:41:37 +0300

    Add converting

--- src/app/ConverterKMs.java
+++ src/app/ConverterKMs.java
@@ -1,5 +1,18 @@
-public class ConverterKMs {
-    static void main() {
+package app;
 
-    }
+public class ConverterKMs {
+    private static final double CONV_KM = 1.60934;
+    public static void main() {
+            System.out.println("Converter App.");
+            System.out.println("App for distance converting");
+            double kms = 5;
+            double miles = ConvKmsToMiles(kms);
+
+            System.out.println("Result is " + miles + " miles");
+
+        }
+
+        private static double ConvKmsToMiles(double kms) {
+            return kms * CONV_KM;
+        }
 }
