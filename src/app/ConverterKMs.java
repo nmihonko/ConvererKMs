540fae5d194cbe98a90171d1d88205856abf5789
Mihonko <118665780+nmihonko@users.noreply.github.com>
2026-04-13 19:02:26 +0300

    Update ConverterKMs

--- src/app/ConverterKMs.java
+++ src/app/ConverterKMs.java
@@ -1,26 +1,27 @@
 package app;
 
 public class ConverterKMs {
+
     private static final double CONV_KM = 1.60934;
 
-    public static void main() {
+    public static void main(String[] args) {
         System.out.println("Converter App.");
         System.out.println("App for distance converting");
+
         double kms = 5;
         double mil = 80;
-        double miles = ConvKmsToMiles(kms);
+
+        double miles = convKmsToMiles(kms);
         double kilometres = convMilesToKms(mil);
 
-        System.out.println("Result is " + miles + " miles and kms " + kilometres + "kms;");
-
+        System.out.println("Result is %.2f miles and %.2f kms".formatted(miles, kilometres));
     }
 
-    private static double ConvKmsToMiles(double kms) {
-        return kms * CONV_KM;
+    private static double convKmsToMiles(double kms) {
+        return kms / CONV_KM;
     }
 
     private static double convMilesToKms(double mil) {
-        return mil / CONV_KM;
+        return mil * CONV_KM;
     }
-
 }
